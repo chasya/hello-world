8b4c80a24fc5b6b9efbfdca9b7aeb5df88dc8488
chasya <38361662+chasya@users.noreply.github.com>
2018-04-20 22:21:39 +0300

    Learn nodejs

---
+++ node.js
@@ -0,0 +1,15 @@
+/* http://learn.javascript.ru/screencast/nodejs */
+
+function User(name) {
+	this.name = name;
+}
+
+User.prototype.hello = function (who) {
+	console.log("Hello, " + who.name);
+}
+
+var vasya = new User("Вася");
+var petya = new User("Петя");
+
+vasya.hello(petya);
+
