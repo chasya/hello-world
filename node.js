b872c55ce990a5a11a9249e47d202e780e8ce88a
chasya <38361662+chasya@users.noreply.github.com>
2018-04-21 22:54:45 +0300

    lesson 6

--- node.js
+++ node.js
@@ -1,15 +1,25 @@
 /* http://learn.javascript.ru/screencast/nodejs */
 
-function User(name) {
-	this.name = name;
+var db = require('./db');
+//db.connect();
+
+var User = require('./user');
+
+function run() {
+	//var vasya = new user.User("Вася");
+	//var petya = new user.User("Петя");
+	
+	var vasya = new User("Вася");
+	var petya = new User("Петя");
+
+	vasya.hello(petya);
+	
+	console.log(db.getPhrase("Run successful"));
 }
 
-User.prototype.hello = function (who) {
-	console.log("Hello, " + who.name);
+if (module.parent) {	//если этот модуль кто-то подключил
+	exports.run = run;
+} else {				//если сервер запущен сам по себе
+	run();
 }
-
-var vasya = new User("Вася");
-var petya = new User("Петя");
-
-vasya.hello(petya);
 
